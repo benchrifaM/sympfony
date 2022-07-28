2c1806a3c0d5ab7ee1c466b7824476a88e6fc527
DevEnver <benchrifa.contact@gmail.com>
2022-07-28 17:02:21 +0200

    Learning symfony : how to use controller, view , and the classes : Response and Request with the parametersBags

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,15 +4,29 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
 {
     #[Route('/test', name: 'app_test')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+       /*  echo $request->getPathInfo();
+        echo "<hr>";
+        echo $request->query->get('info');
+        echo "<hr>";
+        print_r($request->query->all());
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
-        ]);
+        ]); */
+
+
+        $reponse = new Response();
+        $reponse->setContent('hello world');
+        $reponse->headers->set('Content-Type','text/plain');
+        $reponse->setStatusCode(Response::HTTP_NOT_FOUND);
+        $reponse->setCharset('ISO-8859-1');
+        return $reponse;
     }
 }
